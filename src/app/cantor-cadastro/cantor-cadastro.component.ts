84516a9da4699c3e39cabfa20486b394bd580902
cleverson Cordeiro <cleversonkleber@gmail.com>
2025-09-28 10:24:26 -0300

    Videoaula 18 - Formulário de Cadastro - Parte 2

--- src/app/cantor-cadastro/cantor-cadastro.component.ts
+++ src/app/cantor-cadastro/cantor-cadastro.component.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
+class Cantor{
+  nome:string="";
+  pais:string="";
+}
+
 @Component({
   selector: 'app-cantor-cadastro',
   templateUrl: './cantor-cadastro.component.html',
   styleUrls: ['./cantor-cadastro.component.css']
 })
 export class CantorCadastroComponent {
+  paises =['Brasil','Estados Unidos','Inglaterra','Irlanda','Porgual']
+  paisPadrao:string= "Italia";
+
+  cantor = new Cantor()
   salvar(cantorForm: NgForm) {
-    console.log(cantorForm)
+    this.cantor.nome =  cantorForm.value.nomeCantor;
+    this.cantor.pais =  cantorForm.value.nomePais;
+
+    console.log(cantorForm.value)
+    console.log(this.cantor)
   }
 
 
