3fd18f945bf8f06bd049960c5f6f31abb78d7845
cleverson Cordeiro <cleversonkleber@gmail.com>
2025-09-28 11:19:32 -0300

    Videoaula 20 - Validação em Formulários - Parte 2

--- src/app/cantor-cadastro/cantor-cadastro.component.ts
+++ src/app/cantor-cadastro/cantor-cadastro.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
+
 class Cantor{
   nome:string="";
   pais:string="";
@@ -17,11 +18,9 @@ export class CantorCadastroComponent {
 
   cantor = new Cantor()
   salvar(cantorForm: NgForm) {
-    this.cantor.nome =  cantorForm.value.nomeCantor;
-    this.cantor.pais =  cantorForm.value.nomePais;
-
     console.log(cantorForm)
-    // console.log(this.cantor)
+    cantorForm.reset({nomePais:""})
+
   }
 
 
