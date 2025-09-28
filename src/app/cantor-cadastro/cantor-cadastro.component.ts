7c9557a525448c675d5ed8c49bb332a8313e2cfb
cleverson Cordeiro <cleversonkleber@gmail.com>
2025-09-28 09:30:18 -0300

    Videoaula 16 - Pipes

--- src/app/cantor-cadastro/cantor-cadastro.component.ts
+++ src/app/cantor-cadastro/cantor-cadastro.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./cantor-cadastro.component.css']
 })
 export class CantorCadastroComponent {
+  nome = "Cleverson";
+  dataCadastro= new Date(2022, 8,15);
+  valor = 1350.45;
+  valor2=0.3578;
 
 }
